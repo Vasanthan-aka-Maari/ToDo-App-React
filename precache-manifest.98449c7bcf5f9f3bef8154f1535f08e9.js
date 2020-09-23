455f8be865e3b1e500b22bc144bb1deb6eaf8a4e
Vasanthan <vasanth2754@gmail.com>
2020-09-23 10:57:37 +0530

    Updates

---
+++ precache-manifest.98449c7bcf5f9f3bef8154f1535f08e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82a3f732b6260fb2f54e5f6aa0671dc",
+    "url": "/ToDo-App-React/index.html"
+  },
+  {
+    "revision": "fe6a6cff0ea0d0b585da",
+    "url": "/ToDo-App-React/static/css/main.b6c4ba8e.chunk.css"
+  },
+  {
+    "revision": "653081b9d37e49d0bf28",
+    "url": "/ToDo-App-React/static/js/2.4da34c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-App-React/static/js/2.4da34c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6a6cff0ea0d0b585da",
+    "url": "/ToDo-App-React/static/js/main.aa6a910f.chunk.js"
+  },
+  {
+    "revision": "69d708e89aa43d46fcd2",
+    "url": "/ToDo-App-React/static/js/runtime-main.46e4c6b9.js"
+  }
+]);
